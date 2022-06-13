bc5d64e8f6a586e5f5dbd3568e71f76c0404bf43
kaheetonaa@gmail.com <root@DESKTOP-12GFGR1.localdomain>
2022-06-13 18:29:20 +0200

    add donate button

--- sketches/intro.js
+++ sketches/intro.js
@@ -21,7 +21,7 @@ function preload() {
 
 function setup() {
     frameRate(12);
-    dim = min([windowWidth, windowHeight])-60;
+    dim = min([windowWidth, windowHeight]);
     createCanvas(dim, dim);
 }
 
