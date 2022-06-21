cff31e5e3746da6fa2331dd5d13c69801ad38c96
kaheetonaa@gmail.com <root@DESKTOP-12GFGR1.localdomain>
2022-06-22 00:07:28 +0200

    share button and challenge

--- sketches/gol.js
+++ sketches/gol.js
@@ -4,29 +4,34 @@ let pos0, dist0;
 let puzzle_array = [...Array(9).keys()];
 let control = true;
 let start = false;
+let win = false
 let inversion;
 let dim;
 var img = [];
 var sat_img = [];
 let timer = 0;
+var dis, dis_int;
+let challenge
 //https://services.digitalglobe.com/earthservice/tmsaccess/tms/1.0.0/DigitalGlobe:ImageryTileService@EPSG:3857@jpg/19/275635/336717.jpg?connectId=c2cbd3f2-003a-46ec-9e46-26a3996d6484
 
 function preload() {
-  
+  challenge = loadTable('challenge.csv', 'csv', 'header');
   let params = getURLParams();
   let tilex = params.x;
   let tiley = params.y;
   let tilez = params.z;
+  //document.title=tilex
   for (let ty = parseInt(tiley); ty < parseInt(tiley) + 3; ty++) {
     for (let tx = parseInt(tilex); tx < parseInt(tilex) + 3; tx++) {
       img.push(loadImage('https://tile.openstreetmap.org/' + tilez + '/' + tx + '/' + ty + '.png'))
       sat_img.push(loadImage('https://services.digitalglobe.com/earthservice/tmsaccess/tms/1.0.0/DigitalGlobe:ImageryTileService@EPSG:3857@jpg/' + tilez + '/' + tx + '/' + ty + '.jpg?connectId=c2cbd3f2-003a-46ec-9e46-26a3996d6484&flipy=true'))
     }
+
   }
 
 
   //https://services.digitalglobe.com/earthservice/tmsaccess/tms/1.0.0/DigitalGlobe:ImageryTileService@EPSG:3857@jpg/{z}/{x}/{y}.jpg?connectId=c2cbd3f2-003a-46ec-9e46-26a3996d6484&flipy=true
-  img[0]=loadImage('https://raw.githubusercontent.com/kaheetonaa/map8puz/main/images/00.png');
+  img[0] = loadImage('https://raw.githubusercontent.com/kaheetonaa/map8puz/main/images/00.png');
 
   //img = [loadImage('https://raw.githubusercontent.com/kaheetonaa/map8puz/main/images/00.png'), loadImage('https://tile.openstreetmap.org/19/275637/187568.png'), loadImage('https://tile.openstreetmap.org/19/275638/187568.png'), loadImage('https://tile.openstreetmap.org/19/275636/187569.png'), loadImage('https://tile.openstreetmap.org/19/275637/187569.png'), loadImage('https://tile.openstreetmap.org/19/275638/187569.png'), loadImage('https://tile.openstreetmap.org/19/275636/187570.png'), loadImage('https://tile.openstreetmap.org/19/275637/187570.png'), loadImage('https://tile.openstreetmap.org/19/275638/187570.png')];
 }
@@ -35,16 +40,18 @@ function setup() {
   frameRate(12);
   pos0 = puzzle_array.findIndex((element) => element < 1);
   let mouse_pos = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-  
-  dim = min([windowWidth, windowHeight])-60;
+
+  dim = min([windowWidth, windowHeight]) - 60;
   createCanvas(dim, dim);
   puzzle_array = shuffle(puzzle_array);
   inversion = getInvCount(puzzle_array.filter((element) => element > 0));
+
 }
 
 function draw() {
+
   if (frameCount % 12 == 0 && control) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
-    timer ++;
+    timer++;
   }
   if (inversion % 2 == 0) {
     start = true;
@@ -63,8 +70,8 @@ function draw() {
 }
 
 function puzzle_draw() {
-  let dis = dim / 3;
-  let dis_int = dis/20;
+  dis = dim / 3;
+  dis_int = dis / 20;
   //tint(255,127);
   image(sat_img[pos0], decode(pos0)[0] * dis + dis_int, decode(pos0)[1] * dis + dis_int, dis - dis_int, dis - dis_int)
   //tint(255,255);
@@ -109,14 +116,34 @@ function mousePressed() {
     puzzle_array[pos0] = puzzle_array[mpos];
     puzzle_array[mpos] = 0;
   }
+  if (win) {
+    console.log('win!!!')
+    //open twitter
+    if ((mouseX > 0 * dis + dis_int) && (mouseX < 1 * dis + dis_int) &&
+      (mouseY > 1 * dis + dis_int) && (mouseY < 2 * dis + dis_int)) {
+      window.open("https://twitter.com/intent/tweet?text=I%27ve%20just%20win%20Map8puz%20challenge%20in%20only%20%22" + timer + "%22second%21%20Challenge%20yourself%20at%3A%20https%3A%2F%2Fkaheetonaa.github.io%2Fmap8puz%2Findex.html%3Fx%3D17583%26y%3D13209%26z%3D15");
+    }
+    if ((mouseX > 1 * dis + dis_int) && (mouseX < 2 * dis + dis_int) &&
+      (mouseY > 1 * dis + dis_int) && (mouseY < 2 * dis + dis_int)) {
+      window.open("https://www.facebook.com/dialog/share?app_id=366818085567559&href=https%3A%2F%2Fkaheetonaa.github.io%2Fmap8puz%2Findex.html%3Fx%3D17583%26y%3D13209%26z%3D15");
+    }
+    if ((mouseX > 2 * dis + dis_int) && (mouseX < 3 * dis + dis_int) &&
+      (mouseY > 1 * dis + dis_int) && (mouseY < 2 * dis + dis_int)) {
+      window.open("https://www.linkedin.com/sharing/share-offsite/?url=https%3A%2F%2Fkaheetonaa.github.io%2Fmap8puz%2Findex.html%3Fx%3D17583%26y%3D13209%26z%3D15");
+    }
+    //
+  }
 }
 
 function check_win() {
+
   if (getInvCount(puzzle_array.filter((element) => element > 0)) == 0 && pos0 == 0) {
+    win = true
     control = false;
-    textSize(dim/30)
+    textSize(dim / 30)
     fill(255)
-    text('You win in '+timer+' second!', dim/12, dim/9,2*dim/12,5*dim/12);
+    text('You win in ' + timer + ' second!', dim / 12, dim / 9, 2 * dim / 12, 5 * dim / 12);
+    
   }
 }
 
